c02db9ad4491373ab8faf4bd19669472be4c9e4c
ExidnaZZZ <exidna3@bk.ru>
2022-02-11 07:38:06 +0700

    Проект в состоянии Production + рабочий телеграмм-бот

--- Telegram-Bot.py
+++ Telegram-Bot.py
@@ -1,9 +1,11 @@
 #my_first_bot_zzz
+
 from NalogKKM import *
 
 
 import telebot
 bot = telebot.TeleBot("1519526915:AAGnz5H4khS2bD44CZKePX2pN4avbyOpkko", parse_mode=None)
+
 
 from pyowm import OWM
 owm = OWM('8a0792fd900a9fc2244f2db313b1f960')
@@ -12,10 +14,19 @@ owm = OWM('8a0792fd900a9fc2244f2db313b1f960')
 # которая перехватывает все сообщения:
 @bot.message_handler(func=lambda m: True)     #bot.message
 def echo_all(message):                        #bot.message
-    list = main_kkm()
-    for i in list: print(i[2])
+    #list_selected = main_kkm()
+    df = pd.read_excel('data_frame.xlsx', dtype={'RNN': str, 'ZN_KKM': str})
+    delta_day = int(message.text)
+    df_control = selection_df(df, delta_day) # Функция для выявления ККМ, у которых истекает срок действия ФН (запас 20 дней)
+    list = listed_statement(df_control)
+    if len(list) == 0:
+        print('В данный промежуток времени замена ФН не требуется.')
+        return
+    else:
+        volume_print = f'В ближайшие {delta_day} дней требуется провести замену {len(list)} фискальных накопителей: \n'
+        for l_temp in list:
+            volume_print += l_temp[2] + '\n'
+        print(volume_print)
+    bot.reply_to(message, volume_print)       #bot.message
 
-
-    bot.reply_to(message, volume_print)  # bot.message
-
-bot.polling()  # Эта команда запускает самого бота
+bot.polling()                   # Эта команда запускает самого бота
